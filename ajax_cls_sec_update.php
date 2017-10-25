9bc0898129735a013ac3704daa54ee581dc891ee
Ankit Sisodiya <ankit@phppoets.com>
2017-10-25 12:55:51 +0530

    dsu

--- ajax_cls_sec_update.php
+++ ajax_cls_sec_update.php
@@ -55,29 +55,30 @@ include("database.php");
 										<a class="btn btn-xs red" data-toggle="modal" href="#<?php echo $id; ?>">
 									Delete 
 									</a>
-									
-			         <div class="modal fade" id="<?php echo $id; ?>" tabindex="-1" role="basic" aria-hidden="true" style="text-align:left !important">
-								<div class="modal-dialog">
-									<div class="modal-content">
-										<div class="modal-header">
-											<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-											<h4 class="modal-title"><font color="red">Delete Section <?php echo $section; ?> From Class <?php echo $class; ?></font></h4>
+							<form method="post">
+							 <div class="modal fade" id="<?php echo $id; ?>" tabindex="-1" role="basic" aria-hidden="true" style="text-align:left !important">
+										<div class="modal-dialog">
+											<div class="modal-content">
+												<div class="modal-header">
+													<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+													<h4 class="modal-title"><font color="red">Delete Section <?php echo $section; ?> From Class <?php echo $class; ?></font></h4>
+												</div>
+												<div class="modal-body">
+													 You Want To Confirm <font color="red">Delete</font>
+												</div>
+												 
+												<div class="modal-footer">
+													<button type="button" class="btn default" data-dismiss="modal">Close</button>
+													<input type="hidden" name="del_id" value="<?php echo $id; ?>">
+													<button type="submit" name="sub1" class="btn blue">Delete</button>
+												</div>
+												 
+											</div>
+											<!-- /.modal-content -->
 										</div>
-										<div class="modal-body">
-											 You Want To Confirm <font color="red">Delete</font>
-										</div>
-										 
-										<div class="modal-footer">
-											<button type="button" class="btn default" data-dismiss="modal">Close</button>
-											<input type="hidden" name="del_id" value="<?php echo $id; ?>">
-											<button type="submit" name="sub1" class="btn blue">Delete</button>
-										</div>
-										 
-									</div>
-									<!-- /.modal-content -->
-								</div>
-								<!-- /.modal-dialog -->
-				      </div>
+										<!-- /.modal-dialog -->
+							  </div>
+					  </form>
 									</td>
 								
 								 </tr>
